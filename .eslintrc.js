93e6223f88cc549df08abf89906df1f471c122ff
gyc <873261648@qq.com>
2020-06-03 20:31:10 +0800

    打包后的文件以docs形式上传

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,53 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended'
-  ],
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+    "root": false,
+    "env": {
+        "node": true,
+        "browser": true,
+        "es6": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/recommended"
+    ],
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "eqeqeq": "error",
+        "default-case": "error",
+        "dot-notation": "error",
+        "no-alert": "error",
+        "no-else-return": "error",
+        "no-empty-function": "error",
+        "no-extra-label": "error",
+        "no-fallthrough": "error",
+        "no-multi-spaces": "error",
+        "no-useless-return": "error",
+        "require-await": "error",
+        "vars-on-top": "error",
+        "vue/html-indent": [
+            "error",
+            4
+        ],
+        "vue/script-indent": [
+            "error",
+            4
+        ],
+        "vue/html-closing-bracket-newline": [
+            "error",
+            {
+                "singleline": "never",
+                "multiline": "never"
+            }
+        ]
+    },
+    "parser": "vue-eslint-parser",
+    "parserOptions": {
+        "parser": "babel-eslint",
+        "ecmaVersion": 2020
+    }
 }
